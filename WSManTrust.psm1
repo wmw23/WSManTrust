5f35f68701412e8ad34426b63a0eb1d95fd3c33d
Michael Greene <migreene@outlook.com>
2015-11-08 18:37:31 -0600

    First check in
    Module and tests

---
+++ WSManTrust.psm1
@@ -0,0 +1,25 @@
+﻿function Get-WSManTrust {
+    (Get-Item -Path WSMan:\localhost\Client\TrustedHosts | % Value).split(',')
+}
+
+function New-WSManTrust {
+param(
+[string]$hostname
+)
+    Set-Item -Path WSMan:\localhost\Client\TrustedHosts -Value $hostname -Concatenate -Force
+}
+
+function Remove-WSManTrust {
+param(
+[string]$hostname,
+[switch]$all
+)
+    if ($all) {
+        Clear-Item -Path WSMan:\localhost\Client\TrustedHosts -Force
+      }
+    else {
+        $list = Get-WSManTrust
+        $list = $list.replace($hostname+',','').replace($hostname,'')
+        Set-Item -Path WSMan:\localhost\Client\TrustedHosts -Value $list -Force
+    }
+}
